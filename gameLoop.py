60844a8568073563f1ee8c4d743ef12ef7bf3b2d
TeddyGamez <gamezteddyplayz@gmail.com>
2025-02-01 15:36:23 +0400

    Main File Created

---
+++ gameLoop.py
@@ -0,0 +1,4 @@
+import pygame
+
+def tickLoop():
+    print('Ticked!')
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Test')
+import pygame
+import gameLoop
+from sys import exit
+
+screenWidth = 480
+screenHeight = 360
+screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.init()
+max_fps = 60
+pygame.display.set_caption("RPG Collab")
+clock = pygame.time.Clock()
+fileRoot = ""
+
+def Loop():
+    gameLoop.tickLoop()
+    screen.fill((2, 2, 20))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.mixer.music.stop()
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(max_fps)
+
+while True:
+    Loop()
